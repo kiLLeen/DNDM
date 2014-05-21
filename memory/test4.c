42c520216d3954abcb4a7f4f84a1554577a110b1
Neil Killeen <nkilleen@gmail.com>
2014-05-20 23:41:26 -0700

    Test 4

---
+++ memory/test4.c
@@ -0,0 +1,23 @@
+/* CREATED 5-20-14 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "slug.h"
+
+#define MAX_ALLOCATION 200 /* How many allocations */
+#define SIZE_OF_ALLOC 10 /* Size of the allocation */
+
+int main() {
+    void *mem[MAX_ALLOCATION] = { 0 };
+    int i;
+
+    for (i = 0; i < MAX_ALLOCATION; ++i) {
+        mem[i] = malloc(SIZE_OF_ALLOC);
+        printf("Allocating: %p\n", mem[i]);
+    }
+
+    printf("Exiting...\n");
+    return 0;
+}
