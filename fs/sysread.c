8abe9863dac4f4d14adb11b83bb53eb75fc15be2
dlazell <dlazell@ucsc.edu>
2014-05-31 15:39:55 -0700

    initial

---
+++ fs/sysread.c
@@ -0,0 +1,32 @@
+/* CHANGED 5-27-14 */
+
+#include <sys/cdefs.h>
+#include <lib.h>
+#include "namespace.h"
+
+#include <unistd.h>
+
+#ifdef __weak_alias
+__weak_alias(read, _read)
+#endif
+
+ssize_t read(int fd, void *buffer, size_t nbytes)
+{
+  message m;
+
+  m.m1_i1 = fd;
+  m.m1_i2 = nbytes;
+  m.m1_p1 = (char *) buffer;
+  return(_syscall(VFS_PROC_NR, READ, &m));
+}
+
+/* CHANGE START */
+ssize_t metaread(int fd, void *buffer, size_t nbytes) {
+    message m;
+
+    m.m1_i1 = fd;
+    m.m1_i2 = nbytes;
+    m.m1_p1 = (char *)buffer;
+    return(_syscall(VFS_PROC_NR, METAREAD, &m));
+}
+/* CHANGE END */
